2e7d797528923a047ff3ae84acb43ee8394e2e39
Anup Prakash <2000.anupprakash@gmail.com>
2021-01-01 00:56:43 +0530

    Adding the Horoscope Matrix of codechef

--- CodeChefCP/HoroscopeMatrix.cpp
+++ CodeChefCP/HoroscopeMatrix.cpp
@@ -1,55 +1,47 @@
 #include<iostream>
 using namespace std;
 
-#define ll long long 
+#define ll long long
 
 int main(){
-
     ll t;
     cin>>t;
-    while (t--)
-    {
-        ll n,m,q,x,y,v=0;
+    while(t--){
+        int n,m,count=0;
         cin>>n>>m;
-        int arr[n][m];
+        int a[n][m];
 
-        for(int i=0;i<n;i++){
+        for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                cin>>arr[i][j];
+                cin>>a[i][j];
             }
         }
 
-        cin>>q;
-        while(q--){
-            bool flag = false;
-            cin>>x>>y>>v;
-            x-=1;
-            y-=1;
-            arr[x][y]=v;
-            int j;
-            for(int i = x,j = y;   i>=0,j>=0;  i--){ 
-                if(arr[i][j]!=v){
-                    flag = true;
-                    break;
-                }
-                j--;
-            }
-            for(int i = x,j = y;   i>n,j>m;  i++){ 
-                if(arr[i][j]!=v){
-                    flag = true;
-                    break;
-                }
-                j++;
-            }
-            
-            if(flag){
-                cout<<"No"<<endl;
-            }else{
-                cout<<"Yes"<<endl;
+        for(int i = 1;i<n;i++){
+            for(int j = 1;j<m;j++){
+                count+=(a[i-1][j-1]!=a[i][j]);
             }
         }
+
+    int q, x, y, v;
+    cin >> q;
+    while(q--)
+    {
+        cin >> x >> y >> v;
+        x--, y--;
+        if(x - 1 >= 0 and y - 1 >= 0)
+            count -= (a[x][y] != a[x - 1][y - 1]);
+        if(x + 1 < n and y + 1 < m)
+            count -= (a[x][y] != a[x + 1][y + 1]);
+        a[x][y] = v;
+        if(x - 1 >= 0 and y - 1 >= 0)
+            count += (a[x][y] != a[x - 1][y - 1]);
+        if(x + 1 < n and y + 1 < m)
+            count += (a[x][y] != a[x + 1][y + 1]);
+        cout << (count == 0 ? "YES" : "NO") << endl;
+	}
+
     }
-    
 
     return 0;
 }
